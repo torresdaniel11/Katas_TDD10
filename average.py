cf3284bf6ef6c66fda02735139c9628c36182a29
Daniel <d.torres11@uniandes.edu.co>
2018-03-18 00:23:28 -0500

    Paso 20: (Azul) Hacer refactoring-> Mínimo valor del array con string con n elementos

--- average.py
+++ average.py
@@ -16,11 +16,6 @@ class Average:
                response[1] = None
            else:
                response[0]=len(elementos)
-           if len(elementos) == 1 and len(array)>0:
-               response[1] = int(elementos[0])
-           elif len(elementos) == 2:
-               response[1] = min(elementos)
-           elif len(elementos) > 2:
                response[1] = min(elementos)
            return response
 
--- test_average.py
+++ test_average.py
@@ -6,7 +6,7 @@ class TestAverage(TestCase):
         self.assertEqual(Average().arrayNumbers("")[0], 0, "Elementos cadena vacia")
 
     def test_arrayNumbers_elementos_1(self):
-        self.assertEqual(Average().arrayNumbers("3")[0], 1, "Elementos cadena 1")
+        self.assertEqual(Average().arrayNumbers("32")[0], 1, "Elementos cadena 1")
 
     def test_arrayNumbers_elementos_2(self):
         self.assertEqual(Average().arrayNumbers("233, 232")[0], 2, "Elementos cadena 2")
@@ -18,7 +18,7 @@ class TestAverage(TestCase):
         self.assertEqual(Average().arrayNumbers("")[1], None, "Minimo cadena vacia")
 
     def test_arratNumbers_min_1(self):
-        self.assertEqual(Average().arrayNumbers("3")[1], 3, "Minimo cadena 1")
+        self.assertEqual(Average().arrayNumbers("32")[1], 32, "Minimo cadena 1")
 
     def test_arratNumbers_min_2(self):
         self.assertEqual(Average().arrayNumbers("1, 434")[1], 1, "Minimo cadena 2")
