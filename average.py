659a2dbe13189e4b61bbc29857c3c0b54f3d49af
Daniel <d.torres11@uniandes.edu.co>
2018-03-18 00:17:41 -0500

    Paso 17: (Verde) Escribir código-> Mínimo valor del array con string con 2 elementos

--- average.py
+++ average.py
@@ -1,12 +1,25 @@
 class Average:
-   def arrayNumbers(self, array):
-       response = ['length', 'min', 'max', 'average']
-       elementos = array.split(",")
-       if len(array) == 0:
-           response[0]=0
-           response[1] = None
-       else:
-           response[0]=len(elementos)
-       if len(elementos) == 1 and len(array)>0:
-           response[1] = int(elementos[0])
-       return response
+
+    def string_to_num_array(self, array):
+        stringsArray = array.split(",")
+        elementos = list()
+        for i in stringsArray:
+            if i != '':
+                elementos.append(int(i))
+        return elementos
+
+    def arrayNumbers(self, array):
+           response = ['length', 'min', 'max', 'average']
+           elementos = Average().string_to_num_array(array)
+           if len(array) == 0:
+               response[0]=0
+               response[1] = None
+           else:
+               response[0]=len(elementos)
+           if len(elementos) == 1 and len(array)>0:
+               response[1] = int(elementos[0])
+           elif len(elementos) == 2:
+               response[1] = min(elementos)
+           return response
+
+
