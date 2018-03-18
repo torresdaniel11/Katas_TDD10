54f3c17fa3a19fddaef5fee52d28e8f43ac1e965
Daniel <d.torres11@uniandes.edu.co>
2018-03-17 23:34:55 -0500

    Paso 10: (Azul) Hacer refactoring-> número elementos con string n elementos

--- average.py
+++ average.py
@@ -3,9 +3,5 @@ class Average:
        elementos = array.split(",")
        if len(array) == 0 :
            return 0
-       elif len(elementos)==1:
-           return 1
-       elif len(elementos)==2:
-           return 2
        else:
            return len(elementos)
