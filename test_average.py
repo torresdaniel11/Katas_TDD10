6b080381113c0c758e3d7b8dc5702990c085ed2d
Daniel <d.torres11@uniandes.edu.co>
2018-03-17 23:25:56 -0500

    Paso 3: (Roko) Escribir prueba-> número elementos con string 1 elemento

--- test_average.py
+++ test_average.py
@@ -4,3 +4,6 @@ from average import Average
 class TestAverage(TestCase):
     def test_arrayNumbers_elementos_vacio(self):
         self.assertEqual(Average().arrayNumbers(""), 0 , "Elementos cadena vacia")
+
+    def test_arrayNumbers_elementos_1(self):
+        self.assertEqual(Average().arrayNumbers("3"), 1 , "Elementos cadena 1")
