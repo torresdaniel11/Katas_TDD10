79bc3a064167bf6f1bda0f52ee55abe93c7e25ab
Daniel <d.torres11@uniandes.edu.co>
2018-03-17 23:31:22 -0500

    Paso 7: (Azul) Hacer refactoring-> número elementos con string 2 elementos

--- average.py
+++ average.py
@@ -1,8 +1,9 @@
 class Average:
    def arrayNumbers(self, array):
+       elementos = array.split(",")
        if len(array) == 0 :
            return 0
-       elif len(array)==1:
+       elif len(elementos)==1:
            return 1
-       elif len(array)==2:
+       elif len(elementos)==2:
            return 2
