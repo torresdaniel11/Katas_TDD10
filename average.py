8f0b1d32a0aa62ba3ba95929c78f8aaa9a71e83e
Daniel <d.torres11@uniandes.edu.co>
2018-03-17 23:47:27 -0500

    Paso 11: (Rojo) Escribir prueba-> Mínimo valor del array con string vacio

--- average.py
+++ average.py
@@ -1,7 +1,9 @@
 class Average:
    def arrayNumbers(self, array):
+       response = ['length', 'min', 'max', 'average']
        elementos = array.split(",")
-       if len(array) == 0 :
-           return 0
+       if len(array) == 0:
+           response[0]=0
        else:
-           return len(elementos)
+           response[0]=len(elementos)
+       return response
--- test_average.py
+++ test_average.py
@@ -3,13 +3,16 @@ from average import Average
 
 class TestAverage(TestCase):
     def test_arrayNumbers_elementos_vacio(self):
-        self.assertEqual(Average().arrayNumbers(""), 0 , "Elementos cadena vacia")
+        self.assertEqual(Average().arrayNumbers("")[0], 0, "Elementos cadena vacia")
 
     def test_arrayNumbers_elementos_1(self):
-        self.assertEqual(Average().arrayNumbers("3"), 1 , "Elementos cadena 1")
+        self.assertEqual(Average().arrayNumbers("3")[0], 1, "Elementos cadena 1")
 
     def test_arrayNumbers_elementos_2(self):
-        self.assertEqual(Average().arrayNumbers("233, 232"), 2 , "Elementos cadena 2")
+        self.assertEqual(Average().arrayNumbers("233, 232")[0], 2, "Elementos cadena 2")
 
     def test_arrayNumbers_elementos_n(self):
-        self.assertEqual(Average().arrayNumbers("233, 232,23,23,2,2,2,432, 466,6"), 10 , "Elementos cadena n")
+        self.assertEqual(Average().arrayNumbers("233, 232,23,23,2,2,2,432, 466,6")[0], 10, "Elementos cadena n")
+
+    def test_arratNumbers_min_vacio(self):
+        self.assertEqual(Average().arrayNumbers("")[1], None, "Minimo cadena vacia")
