7e432083941cecf74bd079e12717e46688d28851
Daniel <d.torres11@uniandes.edu.co>
2018-03-18 00:50:29 -0500

    Paso 33: (Verde) Escribir código-> Valor promedio del array con string con 1 elemento

--- average.py
+++ average.py
@@ -8,18 +8,23 @@ class Average:
                 elementos.append(int(i))
         return elementos
 
+    def mean(self, numbers):
+        return float(sum(numbers)) / max(len(numbers), 1)
+
     def arrayNumbers(self, array):
            response = ['length', 'min', 'max', 'average']
            elementos = Average().string_to_num_array(array)
            if len(array) == 0:
-               response[0]=0
+               response[0] = 0
                response[1] = None
                response[2] = None
                response[3] = None
            else:
-               response[0]=len(elementos)
+               response[0] = len(elementos)
                response[1] = min(elementos)
                response[2] = max(elementos)
+           if len(elementos) == 1:
+               response[3] = Average().mean(elementos)
            return response
 
 
